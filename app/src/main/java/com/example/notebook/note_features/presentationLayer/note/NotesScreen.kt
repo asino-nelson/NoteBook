5c6b179320b7f524f8839a56d4252e2c742b4c0c
asino-nelson <asinonelson276@gmail.com>
2023-02-11 21:28:49 +0300

    Working on NoteScreen

--- app/src/main/java/com/example/notebook/note_features/presentationLayer/note/NotesScreen.kt
+++ app/src/main/java/com/example/notebook/note_features/presentationLayer/note/NotesScreen.kt
@@ -1,2 +1,19 @@
 package com.example.notebook.note_features.presentationLayer.note
 
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.navigation.NavController
+
+@Composable
+fun NoteScreen(
+    navController: NavController,
+    viewModel: NotesViewModel = hiltViewModel()
+){
+    val state = viewModel.state.value
+    val scaffoldState = rememberScaffoldState
+    val scope = rememberCoroutineScope()
+
+
+
+}
